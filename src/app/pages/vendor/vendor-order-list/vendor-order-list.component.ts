1769aca1195f2e51b6a8fdcd023930dc6a4fcd77
simar2903 <simarpreetk484@gmail.com>
2024-05-16 18:26:04 +0530

    Orders List And Vendor Pannel Color Change

---
+++ src/app/pages/vendor/vendor-order-list/vendor-order-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { OrderListComponent } from '../../account/order-list/order-list.component';
+
+@Component({
+  selector: 'app-vendor-order-list',
+  standalone: true,
+  imports: [OrderListComponent],
+  templateUrl: './vendor-order-list.component.html',
+  styleUrl: './vendor-order-list.component.css'
+})
+export class VendorOrderListComponent {
+
+}

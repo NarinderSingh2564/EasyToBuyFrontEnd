de88c49a714bc1c76ab6cc25184f08c40aad78e5
simar2903 <simarpreetk484@gmail.com>
2024-05-17 16:14:47 +0530

    customer order list

--- src/app/pages/customer/customer-orders/customer-orders.component.ts
+++ src/app/pages/customer/customer-orders/customer-orders.component.ts
@@ -15,36 +15,26 @@ export class CustomerOrdersComponent {
   accountService = inject(AccountService)
 
   orderList: any = []
- 
 
   constructor() {
     this.getOrderList()
   }
 
   getOrderList() {
-      this.orderService.getOrderList(0, this.accountService.getUserId(), "", "").subscribe((result: any) => {
-        this.orderList = result
-      })
-    }
-    
+    this.orderService.getOrderList(this.accountService.getUserId(), 0, "", "").subscribe((result: any) => {
+      this.orderList = result
+    })
   }
 
-  // searchProducts(searchText: string) {
-  //   if (this.accountService.getUserRole() == "Vendor") {
+  searchProducts(searchText: string) {
+    this.orderService.getOrderList(this.accountService.getUserId(), 0, searchText, "").subscribe((result: any) => {
+      this.orderList = result
+    })
+  }
 
-  //     this.header = (this.statusId == 0) ? "All Orders" : (this.statusId == 1) ? "Pending Orders" : (this.statusId == 4) ? "Delivered Orders" : "Cancelled Orders";
-
-  //     var newStatusId = (this.statusId == 0 ? "" : this.statusId == 1 ? '1,3' : this.statusId.toString());
-
-  //     this.orderService.getOrderList(0, this.accountService.getUserId(), searchText, newStatusId).subscribe((result: any) => {
-  //       this.orderList = result
-  //     })
-  //   }
-  //   else {
-  //     this.orderService.getOrderList(this.accountService.getUserId(), 0, searchText, "").subscribe((result: any) => {
-  //       this.orderList = result
-  //     })
-  //   }
-  // }
-  
-
+  filterOrdersByType(event: any) {
+    this.orderService.getOrderList(this.accountService.getUserId(), 0, "", event.target.value).subscribe((result: any) => {
+      this.orderList = result
+    })
+  }
+}

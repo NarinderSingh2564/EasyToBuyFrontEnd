6eb5cf95e788642b213de80f0b507d57f1c88178
arunkumar2553 <arunkumarbloom967@gmail.com>
2024-10-10 17:44:16 -0400

    rating And Review Is Done

--- src/app/pages/customer/customer-orders/customer-orders.component.ts
+++ src/app/pages/customer/customer-orders/customer-orders.component.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AccountService } from '../../../services/account.service';
 import { OrderService } from '../../../services/order.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EasyToBuyHelper } from '../../../helpers/EasyToBuyHelper';
 import { Console } from 'node:console';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-customer-orders',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './customer-orders.component.html',
   styleUrl: './customer-orders.component.css'
 })
@@ -18,25 +19,44 @@ export class CustomerOrdersComponent {
 
   orderService = inject(OrderService)
   accountService = inject(AccountService)
+  productService = inject(ProductService)
   router = inject(Router)
 
+  ratingForm: FormGroup;
+  isFormValid: boolean = false;
+  ratingImageList: any = []
   orderList: any = []
-  orderNumber: string = ""
+  productIds: number = 0
+  btnSubmit:boolean= false
   orderStatusTrackingList: any = []
   currentStatusId: number = 0
   timelineHeight: boolean = false
   baseUrl: string = EasyToBuyHelper.imageBaseUrl;
-  reviewDes: string = '';
+  reviewTitle: string = '';
   ratingNumber : number = 0; 
 
-  constructor() {
-    this.getOrderList()
+
+  constructor(private formBuilder: FormBuilder) {
+    this.getOrderList();
+
+    this.ratingForm = this.formBuilder.group({
+      id: new FormControl(0),
+      reviewTitle: new FormControl("", [Validators.required]),
+      rating: new FormControl("", [Validators.required]),
+      productId: new FormControl("", [Validators.required]),
+      reviewDescription: new FormControl("", [Validators.required]),
+      productRatingImage: new FormControl("", [Validators.required]),
+    })
   }
 
   getOrderList() {
     this.orderService.getOrdersList(this.accountService.getCustomerId(), "", 0, "", "").subscribe((result: any) => {
       this.orderList = result
     })
+  }
+
+  addRatings(){
+    this.isFormValid = false
   }
 
   searchProducts(searchText: string) {
@@ -85,8 +105,8 @@ export class CustomerOrdersComponent {
     this.router.navigate(['product-description', productId])
   }
 
-  getOrderNumber(orderNo: string) {
-    this.orderNumber = orderNo
+  getProductId(Id: any) {
+    this.productIds = Id
   }
 
   getOrderStatusTrackingList(orderNumber: any, statusId: number, variationId: number) {
@@ -117,9 +137,47 @@ export class CustomerOrdersComponent {
   }
 
   mySet(myvalues : any){
-    this.reviewDes = myvalues.reviewTitle;
+    this.reviewTitle = myvalues.reviewTitle;
     this.ratingNumber = myvalues.ratingNumber;
-    console.log(this.reviewDes,this.ratingNumber);
+ }
+
+ selectedImages(event: any){
+  if(event.target.files.length > 3){
+    alert("You can not upload more than 3 images.")
+  }
+  else{
+  
+    this.ratingImageList = []
+    for (var i = 0; i < event.target.files.length; i++) {
+      this.ratingImageList.push(event.target.files[i]);
+    }
+  }
+ }
+
+ RatingAdd(){
+   if( this.isFormValid = true) {
+    this.ratingForm.value.productId = this.productIds;
+    this.ratingForm.value.reviewTitle = this.reviewTitle;
+    const formData = new FormData();
+      formData.set("id", this.ratingForm.value.id != null && this.ratingForm.value.id > 0 ? this.ratingForm.value.id : 0);
+      formData.set("rating",  this.ratingForm.value.rating);
+      formData.set("reviewTitle", this.ratingForm.value.reviewTitle);
+      formData.set("productId", this.ratingForm.value.productId);
+      formData.set("reviewDescription", this.ratingForm.value.reviewDescription);
+      formData.set("createdBy", this.accountService.getCustomerId());
+      for (var i = 0; i < this.ratingImageList.length; i++) {
+        formData.append("productRatingImage", this.ratingImageList[i]);
+      }     
+      this.productService.ratingReviewAdd(formData).subscribe((result:any) => {
+        if (result.status) {
+          alert("Rating Add Sucessful");
+        }
+        else {
+          alert("Error");
+        }
+      });
+    
+    }
  }
 
 }
